4a8109a57a02de26aa11a534f5791606d33b9652
KirillGromov <kirill.gromow@yandex.ru>
2022-10-24 22:46:29 +0300

    git init2

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,9 +1,4 @@
-﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-
-
-void InputNums(){
+﻿void InputNums(){
     int b1, k1, b2, k2;
     Console.WriteLine("Введите b1");
     b1 = int.Parse(Console.ReadLine());
@@ -30,14 +25,3 @@ void ShowRes(int b1, int k1, int b2, int k2){
 }
 
 InputNums();
-
-
-// if ((k1 == k2) && (b1 == b2)){
-//     cout << "Прямые совпадают";
-// }else if (k1==k2){
-//   cout << "Прямые параллельны";
-// }  else {  
-//     x=(b2-b1)/(k1-k2);  
-//     y=(k1*(b2-b1))/(k1-k2)+b1;  
-//     cout << "x=" << x << "y=" << y;  
-// }
